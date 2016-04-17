04a081a80e8a8a8d1ad354d21e93ce8e1847cb5c
Tim Goddard <tim.goddard@aurainfosec.com>
2016-04-17 14:31:12 +1200

    Update Wellington water profile

--- test_brew_salt_optimiser.php
+++ test_brew_salt_optimiser.php
@@ -2,7 +2,7 @@
 
 require_once('brew_salt_optimiser.php');
 
-$wellington_water = array("Ca" => 38, "Mg" => 9, "Na" => 12, "Cl" => 15, "SO4" => 4, "HCO" => 44);
+$wellington_water = array("Ca" => 5, "Mg" => 1, "Na" => 13, "Cl" => 22, "SO4" => 5, "HCO" => 31.2);
 $balanced_profile = array("Ca" => 80, "Mg" => 5, "Na" => 25, "Cl" => 75, "SO4" => 80, "HCO" => 100);
 
 $out = BrewSaltOptimiser::optimise_brewing_salts($wellington_water, $balanced_profile);
