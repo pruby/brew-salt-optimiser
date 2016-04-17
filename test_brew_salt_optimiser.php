6474dd510ac6daaec1aa6330761d27aa4f79e248
Tim Goddard <tim@goddard.net.nz>
2016-04-18 09:41:06 +1200

    Update test_brew_salt_optimiser.php
    Add all salts in order by default.

--- test_brew_salt_optimiser.php
+++ test_brew_salt_optimiser.php
@@ -6,7 +6,7 @@ $wellington_water = array("Ca" => 5, "Mg" => 1, "Na" => 13, "Cl" => 22, "SO4" =>
 $balanced_profile = array("Ca" => 80, "Mg" => 5, "Na" => 25, "Cl" => 75, "SO4" => 80, "HCO" => 100);
 $litres = 30;
 
-$available_salts = array("Table Salt", "Gypsum", "Chalk", "Calcium Chloride", "Epsom Salt");
+$available_salts = array("Gypsum", "Epsom Salt", "Table Salt", "Calcium Chloride", "Magnesium Chloride", "Chalk", "Baking Soda");
 $salts = array();
 foreach ($available_salts as $salt)
 {
